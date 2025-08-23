7cf27cd8451f247143ea48665373bd5136ffca1e
SatoX69 <rahima209zero@gmail.com>
2025-08-23 14:52:58 +0600

    Fixed Tons

--- ose.sh
+++ ose.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 read -r -p "Enter OTP: " TOKEN
 
-if node verify-otp.js "$TOKEN"; then
+if node ~/.ose/verify-otp.js "$TOKEN"; then
   echo "OTP OK"
-  sudo python3 init_attck -i wlan0 -K -w -s
+  sudo python3 ~/.ose/init_attck -i wlan0 -K -w -s
 else
   echo "Invalid OTP"
   exit 1
